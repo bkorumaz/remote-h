0fff6d9e9b63ed4d86e6e02f78c380f1a17d6eee
bkorumaz <bahadir.korumaz@gmail.com>
2020-04-11 09:14:14 +0300

    final 3

--- app/src/main/java/com/tigers/remote_h/ResultEnvironment.java
+++ app/src/main/java/com/tigers/remote_h/ResultEnvironment.java
@@ -1,18 +1,21 @@
 package com.tigers.remote_h;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultEnvironment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_result_environment);
         TextView main_actvity= (TextView)findViewById(R.id.textView2);
+
         main_actvity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,6 +23,5 @@ public class ResultEnvironment extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        setContentView(R.layout.activity_result_environment);
     }
 }
